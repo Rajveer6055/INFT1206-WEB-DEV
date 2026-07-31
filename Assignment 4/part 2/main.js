a887d749b602f1276480a4f5a01ed0caf3e05d91
Rajveer6055 <rajveer.brar@dcmail.ca>
2026-07-31 06:56:29 -0400

    Update main.js Few edits with some errors

--- Assignment 4/part 2/main.js
+++ Assignment 4/part 2/main.js
@@ -4,48 +4,51 @@ const thumbBar = document.querySelector(".thumb-bar");
 const btn = document.querySelector("button");
 const overlay = document.querySelector(".overlay");
 
-// Create a data object
+//Images
+
 const images = [
-  { filename: "pic1.jpg", alt: "Closeup of a human eye" },
-  { filename: "pic2.jpg", alt: "Rock that looks like a wave" },
-  { filename: "pic3.jpg", alt: "Purple and white pansies" },
-  { filename: "pic4.jpg", alt: "Section of wall from a pharaoh's tomb" },
-  { filename: "pic5.jpg", alt: "Large moth on a leaf" },
+    { filename: "pic1.jpg", alt: "Human eye" },
+    { filename: "pic2.jpg", alt: "Rock" },
+    { filename: "pic3.jpg", alt: "Pansies" },
+    { filename: "pic4.jpg", alt: "Tomb" },
+    { filename: "pic5.jpg", alt: "Leaf" }
 ];
 
-// Base URL for the images
-const baseURL =
-  "https://mdn.github.io/shared-assets/images/examples/learn/gallery/";
+//url for all the images
+const baseURL ="";
 
-// Loop through the images
+//Loop for image
+
 for (const image of images) {
   const newImage = document.createElement("img");
-
   newImage.src = `${baseURL}${image.filename}`;
   newImage.alt = image.alt;
-
+  newImage.tabIndex = "0";
   thumbBar.appendChild(newImage);
-
-  // Show the full-size image when a thumbnail is clicked
   newImage.addEventListener("click", updateDisplayedImage);
+  newImage.addEventListener("keydown", (e) => {
+    if (e.code === "Enter") {
+      updateDisplayedImage(e);
+    }
+  });
 }
 
+//
 
-// Update the displayed image
 function updateDisplayedImage(e) {
   displayedImage.src = e.target.src;
   displayedImage.alt = e.target.alt;
 }
 
-// Darken/Lighten button
+//Dark and light mode
+
 btn.addEventListener("click", () => {
   if (btn.classList.contains("dark")) {
-    btn.textContent = "Lighten";
+    btn.textContent = "Light";
     overlay.style.backgroundColor = "rgb(0 0 0 / 0.5)";
   } else {
-    btn.textContent = "Darken";
+    btn.textContent = "Dark";
     overlay.style.backgroundColor = "rgb(0 0 0 / 0)";
   }
-
   btn.classList.toggle("dark");
 });
